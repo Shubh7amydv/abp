58d51456845db777959d747e4bc0f1c3d5433ad3
Shubh7amydv <shubham.2023ug1109@iiitranchi.ac.in>
2025-12-26 21:24:38 +0530

    installed body parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 
 
 const { port } =require ('./config/serverconfig');
 
 const setupAndStartServer= async () =>{
     const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(port, ()=>{
         console.log(`Server started at ${port}`);
     });
