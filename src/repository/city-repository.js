6eaa8ee9ee5659000545a6266d7a76e0e05149ad
Shubh7amydv <shubham.2023ug1109@iiitranchi.ac.in>
2025-12-28 11:42:15 +0530

    completed city repository using CRUD and models function

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,15 +3,65 @@ const { City }= require('../models/index');
 
 
 class CityRepository {          // class is blurprint  and this class groups all DB-related City operations
-  async createCity({ name }) {
-    try {
-      const city = await City.create({ name });
-      return city;
-    } catch (error) {
-      throw { error };
-    }
-  }
 
+      async createCity ({name}) {
+        try {
+          const city= await City.create({name });
+          return city;
+        }
+        catch (error){
+            console.log("something went wrong in repository layer");
+            throw(error);
+        }
+      };
+
+
+
+      async deleteCity(cityId) {
+        try {
+          await City.destroy({
+            where :{
+              id: cityId
+            }
+          });
+          return true;
+        }
+        catch(error) {
+          console.log("something went wrong in repository layer");
+          throw(error)
+        }
+      };
+
+
+
+      async updateCity () {
+        try{
+            const city= await City.update(
+              {data},
+              { where :{
+                id: cityId
+                }
+              });
+            return city;
+        }
+        catch(error){
+          console.log("some error occured");
+          throw(error);
+        }
+      };
+
+
+
+      async getCity (cityId) {
+        try {
+          const city=await City.findByPK(cityId);
+          return city;
+        }
+        catch(error){
+          console.log("some error occured");
+          throw(error);
+        }
+      };
   }
 
 
